4493cb83d0eb20c481efdb20af804bcc51604cb7
Slime-Sama <78178911+prescient-slime@users.noreply.github.com>
2022-10-12 18:14:49 -0500

    updated documentation

--- GoL.cxx
+++ GoL.cxx
@@ -5,9 +5,12 @@
 #include <vector>
 using namespace std;
 
+/*this function initializes the grid as a multidimensional
+vector, and then returns it. The goal will be to initialize,
+populate, and then compute.*/
+
 vector<vector<int> > gridInit(int nrow, int mcol){
     vector<vector<int> > grid(nrow, vector<int>(mcol, 0));
-    //fill(grid.begin(), grid.end(), 0);
     for (int i = 0; i < nrow; i++){
         for (int j = 0; j < mcol; j++){
             cout << grid[i][j] << ", ";
