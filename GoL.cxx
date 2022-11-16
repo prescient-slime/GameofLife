dc0a217adf8dbed2860140dc37fe5f953c775468
Slime-Sama <78178911+prescient-slime@users.noreply.github.com>
2022-11-16 17:54:07 -0600

    Implemented getNeighbors() and stepGame() functions getNeighbors() takes in the grid, row, and column, then calculates neighbors of a given position. stepGame() takes in the grid, and then advances the grid to the next state according tothe rules of Conway's Game of Life.

--- GoL.cxx
+++ GoL.cxx
@@ -27,6 +27,27 @@ vector<vector<int> > gridInit(int nrow, int mcol){
     return grid;
 }
 
+int getNeighbors(vector<vector<int> > grid, int row, int col){
+    int topn [3]= {grid[row - 1][col - 1], grid[row - 1][col], grid[row - 1][col + 1]};
+    int lr [2] = {grid[row][col - 1], grid[row][col + 1]};
+    int botn [3] = {grid[row + 1][col - 1], grid[row + 1][col], grid[row + 1][col + 1]};
+    int neighbors = 0;
+    for (int i = 0; i < 3; i++){
+        if (topn[i] == 1){
+            neighbors++;
+        }
+        if (botn[i] == 1){
+            neighbors++;
+        }
+    }
+    for (int i = 0; i < 2; i++){
+        if (lr[i] == 1){
+            neighbors++;
+        }
+    }
+    return neighbors;
+}
+
 void printGrid(vector<vector<int> > grid){
     for (int i = 0; i < grid.size(); i++){
         for (int j = 0; j < grid[i].size(); j++){
@@ -43,7 +64,23 @@ void init(){
 }
 
 void stepGame(vector<vector<int> > &grid){
-    
+    int* n = new int(0);
+    for (int i = 0; i < grid.size(); i++){
+        for (int j = 0; j < grid[i].size(); j++){
+            //live cell rules
+            if (grid[i][j] == 1){
+                if (getNeighbors(grid, i, j) < 2 || getNeighbors(grid, i, j) > 3){
+                    grid[i][j] = 0;
+                }
+            }
+            // dead cell rules
+            if (grid[i][j] == 0){
+                if (getNeighbors(grid, i, j) > 3){
+                    grid[i][j] = 1;
+                }
+            }
+        }
+    }
 }
 
 void readGrid(vector<vector<int> > grid){
@@ -64,6 +101,7 @@ int main(int argc, char* argv[]){
     glutInitWindowSize(WIDTH, HEIGHT);
     glutCreateWindow("Conway's Game of Life");
     init();
+    glutDisplayFunc(display);
     //printGrid(grid);
     return 0;
 }
