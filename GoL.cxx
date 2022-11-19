a54219bb83ce783edaad40216b76d34b7c1e9791
Slime-Sama <78178911+prescient-slime@users.noreply.github.com>
2022-11-19 10:25:45 -0600

    i-... it's working.... IT'S WORKINGGGG

--- GoL.cxx
+++ GoL.cxx
@@ -1,113 +1,127 @@
-#include <stdio.h>
 #include <iostream>
-#include <stdlib.h>
-#include <array>
-#include <vector>
+#include <random>
+#include <time.h>
 #include <GL/freeglut.h>
 #include <GLUT/glut.h>
+
+
+#define WIDTH 800
+#define HEIGHT 600
+
 using namespace std;
 
-/*this function initializes the grid as a multidimensional
-vector, and then returns it. The goal will be to initialize,
-populate, and then compute.*/
+bool board [HEIGHT][WIDTH] = {false};
 
-int WIDTH = 800;
-int HEIGHT = 600;
-
-
-vector<vector<int> > gridInit(int nrow, int mcol){
-    vector<vector<int> > grid(nrow, vector<int>(mcol, 0));
-    for (int i = 0; i < nrow; i++){
-        for (int j = 0; j < mcol; j++){
-            int n = rand() & 1;
-            grid[i][j] = n;
-        }
-        
-    }
-    return grid;
-}
-
-int getNeighbors(vector<vector<int> > grid, int row, int col){
-    int neighbors = 0;
-    if (row < HEIGHT && col < WIDTH && row > 0 && col > 0){
-        neighbors += grid.at(row - 1).at(col - 1);
-        neighbors += grid.at(row - 1).at(col);
-        neighbors += grid.at(row - 1).at(col + 1);
-        neighbors += grid.at(row).at(col - 1);
-        neighbors += grid.at(row).at(col + 1);
-        neighbors += grid.at(row + 1).at(col - 1);
-        neighbors += grid.at(row + 1).at(col);
-        neighbors += grid.at(row + 1).at(col + 1);
-    }
-    
-    return neighbors;
-}
-
-void printGrid(vector<vector<int> > grid){
-    for (int i = 0; i < grid.size(); i++){
-        for (int j = 0; j < grid[i].size(); j++){
-            cout << grid[i][j] << ", ";
+void printGame(bool(grid)[HEIGHT][WIDTH]){
+    for (int i = 0; i < HEIGHT; i++){
+        for (int j = 0; j < WIDTH; j++){
+            cout << grid[i][j];
         }
         cout << endl;
     }
 }
 
-vector<vector<int> > game = gridInit(HEIGHT, WIDTH);
+void initGame(bool(&grid)[HEIGHT][WIDTH]){
+    srand( time(NULL) ); //Randomize seed initialization
+    for (int i = 0; i < HEIGHT; i++){
+        for (int j = 0; j < WIDTH; j++){
+            int randNum = rand() % 2; // Generate a random number between 0 and 1
+            grid[i][j] = (bool)randNum;
+        }
+    }
+}
+
+
+
+void draw_point(int x, int y, int size){
+    glColor3f(1, 1, 1);
+    glPointSize(size);
+    glBegin(GL_POINTS);
+        glVertex2f(x, y);
+    glEnd();
+}
+
+void drawGame(bool(grid)[HEIGHT][WIDTH]){
+    for (int i = 0; i < HEIGHT; i++){
+        for (int j = 0; j < WIDTH; j++){
+            if (grid[i][j]){
+                //draw_point(j, i, 1);
+                glColor3f(1, 1, 1);
+                glBegin(GL_POINTS);
+                    glVertex2i(j, i);
+                glEnd();
+            }
+        }
+    }
+    glutPostRedisplay();
+}
+
+
+void stepGame(bool(&grid)[HEIGHT][WIDTH]){
+    int neighbors = 0;
+    int dead = 0;
+    bool swap [HEIGHT][WIDTH] = {false};
+    for (int i = 0; i < HEIGHT; i++){
+        for (int j = 0; j < WIDTH; j++){
+            neighbors = int(
+                        grid[(i - 1) % HEIGHT][(j - 1) % WIDTH] +
+                        grid[(i - 1) % HEIGHT][(j)] +
+                        grid[(i - 1) % HEIGHT][(j + 1) % WIDTH] +
+                        grid[(i)][(j - 1) % WIDTH] +
+                        grid[(i)][(j + 1) % WIDTH] +
+                        grid[(i + 1) % HEIGHT][(j - 1) % WIDTH] +
+                        grid[(i + 1) % HEIGHT][(j)] + 
+                        grid[(i + 1) % HEIGHT][(j + 1) % WIDTH]
+            );
+            if (grid[i][j]){
+                if (neighbors > 3 || neighbors < 2){
+                    swap[i][j] = false;
+                }
+                dead++;
+            }
+            if (!grid[i][j]){
+                if (neighbors == 3){
+                    swap[i][j] = true;
+                }
+            }
+        }
+    }
+    /*for (int i = 0; i < HEIGHT; i++){
+        for (int j = 0; j < WIDTH; j++){
+            grid[i][j] = swap[i][j];
+        }
+    }*/
+    std::swap(swap, grid);
+    if (dead > 0){
+        initGame(grid);
+    }
+}
+
+void display(){
+    glClear(GL_COLOR_BUFFER_BIT);
+    drawGame(board);
+    stepGame(board);
+    
+    glFlush();
+    //glFinish();
+}
 
 void init(){
     gluOrtho2D(0, WIDTH, 0, HEIGHT);
 }
 
-void stepGame(vector<vector<int> > &grid){
-    int* n = new int(0);
-    for (int i = 0; i < grid.size(); i++){
-        for (int j = 0; j < grid[i].size(); j++){
-            //live cell rules
-            if (grid[i][j] == 1){
-                try{
-                    if (getNeighbors(grid, i, j) < 2 || getNeighbors(grid, i, j) > 3){
-                    grid[i][j] = 0;
-                    }
-                }
-                catch(const std::out_of_range & ex){
-                    cout << "out of range exception caught" << ex.what();
-                }
-                
-            }
-            // dead cell rules
-            if (grid[i][j] == 0){
-                if (getNeighbors(grid, i, j) > 3){
-                    grid[i][j] = 1;
-                }
-            }
-        }
-    }
-}
-
-void readGrid(vector<vector<int> > grid){
-
-}
-
-void display(){
-    glClear(GL_COLOR_BUFFER_BIT);
-    stepGame(game);
-    readGrid(game);
-    glFlush();
-    glFinish();
-}
-
-
 int main(int argc, char* argv[]){
     glutInit(&argc, argv);
-    glutInitWindowSize(WIDTH, HEIGHT);
+    glutInitWindowSize(800, 600);
     glutCreateWindow("Conway's Game of Life");
     init();
+
     glutDisplayFunc(display);
-    cout << "initial state: " << endl;
-    printGrid(game);
-    cout << "stepping game: " << endl;
-    stepGame(game);
-    cout << "game stepped: " << endl;
-    printGrid(game);
+
+
+
+    glutMainLoop();
+    
+    
     return 0;
 }
