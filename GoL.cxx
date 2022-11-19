76c91134e21beaf78d2f10a5fd3449baa48d2a99
Slime-Sama <78178911+prescient-slime@users.noreply.github.com>
2022-11-19 09:52:19 -0600

    can't get it to work

--- GoL.cxx
+++ GoL.cxx
@@ -1,126 +1,92 @@
+#include <array>
 #include <iostream>
-#include <random>
-#include <time.h>
+//#include <GLUT/glut.h>
 #include <GL/freeglut.h>
-#include <GLUT/glut.h>
-
-
-#define WIDTH 800
-#define HEIGHT 600
+#include <random>
 
 using namespace std;
 
-bool board [HEIGHT][WIDTH] = {false};
+const size_t HEIGHT = 600;
+const size_t WIDTH = 800;
+const float CELL_COLOR[3] = {1.0, 1.0, 1.0};
 
-void printGame(bool(grid)[HEIGHT][WIDTH]){
-    for (int i = 0; i < HEIGHT; i++){
-        for (int j = 0; j < WIDTH; j++){
-            cout << grid[i][j];
-        }
-        cout << endl;
+void init(){
+    gluOrtho2D(0, WIDTH, HEIGHT, 0);
+}
+
+array<array<bool, HEIGHT>, WIDTH > boardInit(array<array<bool, HEIGHT>, WIDTH > &grid){
+    for (auto &row : grid){
+        generate(row.begin(), row.end(), []() { return rand() % 10 == 0 ? 1: 0; });
     }
 }
 
-void initGame(bool(&grid)[HEIGHT][WIDTH]){
-    srand( time(NULL) ); //Randomize seed initialization
-    for (int i = 0; i < HEIGHT; i++){
-        for (int j = 0; j < WIDTH; j++){
-            int randNum = rand() % 2; // Generate a random number between 0 and 1
-            grid[i][j] = (bool)randNum;
-        }
-    }
-}
-
-
-
-void draw_point(int x, int y, int size){
-    glColor3f(1, 1, 1);
-    glPointSize(size);
-    glBegin(GL_POINTS);
-        glVertex2f(x, y);
-    glEnd();
-}
-
-void drawGame(bool(grid)[HEIGHT][WIDTH]){
-    for (int i = 0; i < HEIGHT; i++){
-        for (int j = 0; j < WIDTH; j++){
-            if (grid[i][j]){
-                //draw_point(j, i, 1);
-                glColor3f(1, 1, 1);
-                glBegin(GL_POINTS);
-                    glVertex2i(j, i);
-                glEnd();
-            }
-        }
-    }
-}
-
-
-void stepGame(bool(&grid)[HEIGHT][WIDTH]){
+int countNeighbors(array<array<bool, HEIGHT>, WIDTH> grid, int x, int y){
     int neighbors = 0;
-    int dead = 0;
-    bool swap [HEIGHT][WIDTH] = {false};
-    for (int i = 0; i < HEIGHT; i++){
-        for (int j = 0; j < WIDTH; j++){
-            neighbors = int(
-                        grid[(i - 1) % HEIGHT][(j - 1) % WIDTH] +
-                        grid[(i - 1) % HEIGHT][(j)] +
-                        grid[(i - 1) % HEIGHT][(j + 1) % WIDTH] +
-                        grid[(i)][(j - 1) % WIDTH] +
-                        grid[(i)][(j + 1) % WIDTH] +
-                        grid[(i + 1) % HEIGHT][(j - 1) % WIDTH] +
-                        grid[(i + 1) % HEIGHT][(j)] + 
-                        grid[(i + 1) % HEIGHT][(j + 1) % WIDTH]
-            );
-            if (grid[i][j]){
-                if (neighbors > 3 || neighbors < 2){
-                    swap[i][j] = false;
-                }
-                dead++;
-            }
-            if (!grid[i][j]){
-                if (neighbors == 3){
-                    swap[i][j] = true;
-                }
-            }
-        }
-    }
-    /*for (int i = 0; i < HEIGHT; i++){
-        for (int j = 0; j < WIDTH; j++){
-            grid[i][j] = swap[i][j];
-        }
-    }*/
-    std::swap(swap, grid);
-    if (dead > 0){
-        initGame(grid);
-    }
+    neighbors += (int)(
+        grid[(y - 1)%HEIGHT][(x - 1)%WIDTH] +
+        grid[(y - 1)%HEIGHT][(x)%WIDTH] +
+        grid[(y - 1)%HEIGHT][(x + 1)%WIDTH] +
+        grid[(y)][(x - 1)] +
+        grid[(y)][(x + 1)] +
+        grid[(y + 1)%HEIGHT][(x - 1)%WIDTH] +
+        grid[(y + 1)%HEIGHT][(x)%WIDTH] +
+        grid[(y + 1)%HEIGHT][(x + 1)%WIDTH]
+    );
+    return neighbors;
 }
+void stepGame(array<array<bool, HEIGHT>, WIDTH> &grid){
+    int neighbors = 0;
+    array<array<bool, HEIGHT>, WIDTH> swapGrid = {false};
+    for (int y = 0; y < HEIGHT; y++){
+        for (int x = 0; x < WIDTH; x++){
+            neighbors = countNeighbors(grid, x, y);
+            if (grid[y][x]){
+                if (neighbors > 3 || neighbors < 2){
+                    swapGrid[y][x] = false;
+                }
+            }
+            else if (!grid[y][x]){
+                if (neighbors == 3){
+                    swapGrid[y][x] = true;
+                }
+            }
+        }
+    }
+    swap(grid, swapGrid);
+}
+
+void drawGame(array<array<bool, HEIGHT>,WIDTH> grid, const float color[3]){
+    glPointSize(1);
+    glColor3fv(color);
+    glBegin(GL_POINTS);
+        for (int y = 0; y < HEIGHT; y++){
+            for (int x = 0; x < WIDTH; x++){
+                if (grid[y][x]){
+                    glVertex2d(y, x);
+                }
+            }
+        }
+    glEnd();
+    glutPostRedisplay();
+}
+
+array<array<bool, HEIGHT>,WIDTH> game = boardInit(game);
 
 void display(){
     glClear(GL_COLOR_BUFFER_BIT);
-    drawGame(board);
-    stepGame(board);
-    
+    stepGame(game);
+    drawGame(game, CELL_COLOR);
     glFlush();
-    //glFinish();
+    
+    glFinish();
 }
 
-void init(){
-    gluOrtho2D(0, WIDTH, 0, HEIGHT);
-}
-
-int main(int argc, char* argv[]){
+int main(int argc, char* argv []){
     glutInit(&argc, argv);
-    glutInitWindowSize(800, 600);
+    glutInitWindowSize(WIDTH, HEIGHT);
     glutCreateWindow("Conway's Game of Life");
     init();
-
     glutDisplayFunc(display);
-
-
-
     glutMainLoop();
-    
-    
     return 0;
 }
